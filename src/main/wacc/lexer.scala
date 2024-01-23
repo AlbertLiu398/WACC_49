32240998e9e3879861445d6efca5c06aa7c5b0fd
hz3322 <hanxue.zhao22@imperial.ac.uk>
2024-01-23 19:59:25 +0000

    feat : add keywords set for lexer object

--- src/main/wacc/lexer.scala
+++ src/main/wacc/lexer.scala
@@ -7,10 +7,26 @@ import parsley.token.descriptions._
 object lexer {
     private val desc = LexicalDesc.plain.copy(
         // your configuration goes here
+        keywords =  set("int ", "bool ", "char ", "string ", "pair ", "null ", "skip", "read ", "free ", "return ", 
+                "exit ", "print", "println", "if ", "then ", "else ", "fi", "while ", "do ", "is",
+                "done", "begin", "end", "call", "fst ", "snd ", "newpair", "true", "false",
+                ";" , "(", ")", "{", "}", "[", "]", ",",
+                "!","-", , "len", "ord", "chr", 
+                "+", "-", "*", "/", "%","<", ">", "<=", ">=", "=", "==", "!=","&&", "||")
     )
+    
     private val lexer = new Lexer(desc)
 
     val integer = lexer.lexeme.integer.decimal
     val implicits = lexer.lexeme.symbol.implicits
     def fully[A](p: Parsley[A]): Parsley[A] = lexer.fully(p)
 }
+// input: String
+// output: List[String(token)]
+// example:
+// input: int x = 1;
+// output: ["int", "x", "=", "1", ";"]
+
+
+
+    

e5235a2ad79987c960ab47615796c0b233061151
gs1322 <gs1322@ic.ac.uk>
2024-01-25 12:08:22 +0000

    feat: implemented most of lexer

--- src/main/wacc/lexer.scala
+++ src/main/wacc/lexer.scala
@@ -1,31 +1,79 @@
 package wacc
 
 import parsley.Parsley
-import parsley.token.Lexer
+import parsley.token.{Lexer, predicate}
 import parsley.token.descriptions._
+import scala.collection.mutable.ListBuffer
 
 object lexer {
     private val desc = LexicalDesc.plain.copy(
         // your configuration goes here
-        keywords =  Set("int ", "bool ", "char ", "string ", "pair ", "null ", "skip", "read ", "free ", "return ", 
-                "exit ", "print", "println", "if ", "then ", "else ", "fi", "while ", "do ", "is",
-                "done", "begin", "end", "call", "fst ", "snd ", "newpair", "true", "false",
-                ";" , "(", ")", "{", "}", "[", "]", ",",
-                "!","-", "len", "ord", "chr", 
-                "+", "-", "*", "/", "%","<", ">", "<=", ">=", "=", "==", "!=","&&", "||")
+        nameDesc = NameDesc.plain.copy(
+            identifierStart = predicate.Basic(_.isLetterOrDigit),
+            identifierLetter = predicate.Basic(_.isLetterOrDigit),
+        ),
+        symbolDesc = SymbolDesc.plain.copy(
+            caseSensitive = true,
+            hardKeywords = Set("null ", "skip", "read ", "free ", "return ", "exit ", "print", "println", 
+            "if ", "then ", "else ", "fi", "while ", "do ", "is","done", "begin", "end", "call", "fst ", 
+            "snd ", "newpair", "true", "false",";" , "(", ")", "{", "}", "[", "]", ","),
+            hardOperators = Set("!","-", "len", "ord", "chr", 
+            "+", "-", "*", "/", "%","<", ">", "<=", ">=", "=", "==", "!=","&&", "||"),
+
+        ),
+        spaceDesc = SpaceDesc.plain.copy(
+            lineCommentStart = "//",
+            lineCommentAllowsEOF = true,
+            multiLineCommentStart = "/*", 
+            multiLineCommentEnd = "*/",
+            multiLineNestedComments = false,
+            whitespaceIsContextDependent = false,
+        ),
+        numericDesc = numeric.NumericDesc.plain.copy(
+            literalBreakChar = numeric.BreakCharDesc.NoBreakChar,
+            leadingDotAllowed = false,
+            trailingDotAllowed = false,
+            leadingZerosAllowed = true,
+            positiveSign = numeric.PlusSignPresence.Optional,
+            integerNumbersCanBeHexadecimal = true,
+
+            //
+            integerNumbersCanBeOctal = false, 
+
+            integerNumbersCanBeBinary = false,
+            realNumbersCanBeHexadecimal = false,
+            realNumbersCanBeOctal = false,
+            realNumbersCanBeBinary = false,
+            hexadecimalLeads = Set('x', 'X'),
+        ),
+        textDesc = text.TextDesc.plain.copy(
+            escapeSequences = text.EscapeDesc.plain.copy(
+                escBegin = '\\',
+            ),
+            characterLiteralEnd = '\'',
+            stringEnds = Set(("\"", "\"")),
+            multiStringEnds = Set(("\"\"\"", "\"\"\"")),
+            // graphicCharacter = "",
+        ),
+
     )
-    
+
+    val keywords: Set[String] = Set(
+        "int ", "bool ", "char ", "string ", "pair ", "null ", "skip", "read ", "free ", "return ", 
+        "exit ", "print", "println", "if ", "then ", "else ", "fi", "while ", "do ", "is",
+        "done", "begin", "end", "call", "fst ", "snd ", "newpair", "true", "false",
+        ";" , "(", ")", "{", "}", "[", "]", ",",
+        "!","-", "len", "ord", "chr", 
+        "+", "-", "*", "/", "%","<", ">", "<=", ">=", "=", "==", "!=","&&", "||"
+    )
+
+
     private val lexer = new Lexer(desc)
 
     val integer = lexer.lexeme.integer.decimal
     val implicits = lexer.lexeme.symbol.implicits
     def fully[A](p: Parsley[A]): Parsley[A] = lexer.fully(p)
 }
-// input: String
-// output: List[String(token)]
-// example:
-// input: int x = 1;
-// output: ["int", "x", "=", "1", ";"]
 
 
 

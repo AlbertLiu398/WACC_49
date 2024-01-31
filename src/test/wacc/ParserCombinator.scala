6af7c9952d94199f3ab35a17e9c460ec22d49018
Richard Li <rl2022@ic.ac.uk>
2024-01-31 15:30:02 +0000

    feat(test): init test implementation

---
+++ src/test/wacc/ParserCombinator.scala
@@ -0,0 +1,25 @@
+import parsley.{Failure, Result, Success}
+
+import scala.language.implicitConversions
+
+class ParserCombinatorTest extends ParsleyTest { 
+// ----------- Statement Parser Tests -----------
+
+
+
+
+// ----------- Expression Parser Tests -----------
+
+
+
+
+// ----------- Type Parser Tests -----------
+
+
+
+}
+
+
+
+
+

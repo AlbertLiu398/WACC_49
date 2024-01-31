b81329f0dc8dea3adff932115748941da04309c7
hz3322 <hz3322@ic.ac.uk>
2024-01-31 16:21:37 +0000

    feat: add specific test case [hz3322, gs1322]

--- src/test/wacc/ParserCombinator.scala
+++ src/test/wacc/ParserCombinator.scala
@@ -1,24 +1,92 @@
-import parsley.{Failure, Result, Success}
+package wacc
 
+import parsley.{Failure, Result, Success}
+import ast._
+import parser._
 import scala.language.implicitConversions
 
-class ParserCombinatorTest extends ParsleyTest { 
+class ParserCombinatorTest extends ParserTest { 
 // ----------- Statement Parser Tests -----------
+it should "parse statement" in {
+    // Skip statement
+    parser.parse("skip") shouldBe Success(Skip)
 
+    // Assignment statement
+    parser.parse("x = 5") shouldBe Success(Assignment(IdentLValue(Ident("x")), ExprRValue(IntLiter(5))))
 
+    // If statement
+    parser.parse("if true then skip else skip") shouldBe Success(If(BoolLiter(true), Skip, Skip))
 
+    // While statement
+    parser.parse("while x do skip done") shouldBe Success(While(Ident("x"), Skip))
 
-// ----------- Expression Parser Tests -----------
+    // Print statement
+    parser.parse("print x") shouldBe Success(Print(Ident("x"), false))
 
+    // Println statement    
+    parser.parse("println x") shouldBe Success(Print(Ident("x"), true))
 
+    // Begin statement
+    parser.parse("begin skip end") shouldBe Success(Begin(Skip))
 
+    // Read statement
+    parser.parse("read x") shouldBe Success(Read(IdentLValue(Ident("x"))))
 
-// ----------- Type Parser Tests -----------
+    // Free statement
+    parser.parse("free x") shouldBe Success(Free(Ident("x")))
+
+    // Return statement
+    parser.parse("return x") shouldBe Success(Return(Ident("x")))
+
+    // Exit statement
+    parser.parse("exit x") shouldBe Success(Exit(Ident("x")))
+
+    // New assignment statement
+    parser.parse("int x = 5") shouldBe Success(NewAssignment(BaseType("int"), Ident("x"), ExprRValue(IntLiter(5))))
+
+    // New pair assignment statement
+    parser.parse("pair(int, bool) x = newpair(1, true)") shouldBe Success(NewAssignment(PairType(BaseTypeElem("int"), BaseTypeElem("bool")), Ident("x"), NewPairRValue(IntLiter(1), BoolLiter(true))))
+
+    // // Seq statement
+    // parser.parse("skip; skip") shouldBe Success(SeqStmt(Skip, Skip))
+
+    // // Seq statement
+    // parser.parse("skip; skip; skip") shouldBe Success(SeqStmt(Skip, SeqStmt(Skip, Skip)))
 
 
 
 }
+// ----------- Expression Parser Tests -----------
 
+  it should "parse expression" in {
+
+    // Literal expression
+    parser.parse("123") shouldBe Success(IntLiter(123))
+
+    // Binary operation
+    parser.parse("1 + 2") shouldBe Success(BinaryOperation(BOper("+"), IntLiter(1), IntLiter(2)))
+
+    // Unary operation
+    parser.parse("!true") shouldBe Success(UnaryOperation(UOper("!"), BoolLiter(true)))
+
+    // atom expression
+    
+
+  }
+
+// ----------- Type Parser Tests -----------
+  it should "parse type" in {
+
+    // Base type
+    parser.parse("int") shouldBe Success(BaseType("int"))
+
+    // Array type
+    parser.parse("int[]") shouldBe Success(ArrayType(BaseType("int")))
+
+    // Pair type
+    parser.parse("pair(int, bool)") shouldBe Success(PairType(BaseTypeElem("int"), BaseTypeElem("bool")))
+  }
+}
 
 
 
--- src/test/wacc/ParserTest.scala
+++ src/test/wacc/ParserTest.scala
@@ -1,5 +1,7 @@
+package wacc
+
 import org.scalatest.Assertions
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-abstract class ParsleyTest extends AnyFlatSpec with Matchers with Assertions
+abstract class ParserTest extends AnyFlatSpec with Matchers with Assertions

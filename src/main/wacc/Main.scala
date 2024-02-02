a884149ecf5d216e27138c72477ef40cb06f7451
gs1322 <gs1322@ic.ac.uk>
2024-02-02 18:59:16 +0000

    fix(main): switch the args to be a file path[gs1322]

--- src/main/wacc/Main.scala
+++ src/main/wacc/Main.scala
@@ -6,18 +6,18 @@ object Main {
     def main(args: Array[String]): Unit = {
         println("hello WACC!")
 
-        
-        val filePath = "file.txt"
-        val fileContents: String = Source.fromFile(filePath).mkString
-        println(fileContents)
-        Source.fromFile(filePath).close()
-
         args.headOption match {
-            case Some(expr) => parser.parse(expr) match {
-                case Success(x) => println(s"$expr = $x")
+            case Some(filePath) =>
+                val fileContents: String = Source.fromFile(filePath).mkString
+                Source.fromFile(filePath).close()
+
+                parser.parse(fileContents) match {
+                case Success(x) => 
+                    println("file content is")
+                    println(s"$fileContents = $x")
                 case Failure(msg) => println(msg)
-            }
-            case None => println("please enter an expression")
+                }
+            case None => println("please enter a file name")
         }
     }
 }

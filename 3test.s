eb56fb8230ef2586b33d0d558f5f0a0489cf80e6
hz3322 <hanxue.zhao22@imperial.ac.uk>
2024-02-29 23:51:54 +0000

    fix [CodeGen] : remove cbz and load in Read() [hz]

---
+++ 3test.s
@@ -0,0 +1,148 @@
+.data
+	.word 25
+.L.str0:
+	.asciz "Please input an integer: "
+	.word 2
+.L._printi_str0:
+	.asciz "%d"
+	.word 34
+.L.str3:
+	.asciz "(enter Y for 'yes' and N for 'no')"
+	.word 4
+.L._prints_str0:
+	.asciz "%.*s"
+	.word 39
+.L.str2:
+	.asciz "Do you want to continue entering input?"
+	.word 12
+.L.str1:
+	.asciz "echo input: "
+	.word 3
+.L._readc_str0:
+	.asciz " %c"
+	.word 0
+.L._println_str0:
+	.asciz ""
+	.word 2
+.L._readi_str0:
+	.asciz "%d"
+.align 4
+.text
+.global main
+main:
+	  stp fp, lr, [sp, #-16]!
+      stp x19, x20, [sp, #-16]!
+      mov fp, sp 
+      mov x8, #89 
+      mov x19, x8 
+      mov x8, #0 
+      mov x20, x8 
+      b .L0 
+.L1:
+	  adrp x8, .L.str0 
+      add x8, x8, :lo12:.L.str0 
+      stp x8, xzr, [sp, #-16]!
+      ldp x8, xzr, [sp], #16
+      mov x8, x8 
+      mov x0, x8 
+      bl _prints 
+      mov x8, x20 
+      mov x0, x8 
+      bl _readi 
+      mov x16, x0 
+      mov x8, x16 
+      mov x20, x8 
+      adrp x8, .L.str1 
+      add x8, x8, :lo12:.L.str1 
+      stp x8, xzr, [sp, #-16]!
+      ldp x8, xzr, [sp], #16
+      mov x8, x8 
+      mov x0, x8 
+      bl _prints 
+      mov x8, x20 
+      mov x0, x8 
+      bl _printi 
+      bl _println 
+      adrp x8, .L.str2 
+      add x8, x8, :lo12:.L.str2 
+      stp x8, xzr, [sp, #-16]!
+      ldp x8, xzr, [sp], #16
+      mov x8, x8 
+      mov x0, x8 
+      bl _prints 
+      bl _println 
+      adrp x8, .L.str3 
+      add x8, x8, :lo12:.L.str3 
+      stp x8, xzr, [sp, #-16]!
+      ldp x8, xzr, [sp], #16
+      mov x8, x8 
+      mov x0, x8 
+      bl _prints 
+      bl _println 
+      mov x8, x19 
+      mov x0, x8 
+      bl _readc 
+      mov x16, x0 
+      mov x8, x16 
+      mov x19, x8 
+.L0:
+	mov x8, x19 
+	mov x9, x8 
+	mov x8, #78 
+	cmp x9, x8 
+	cset x8, NE 
+	b.NE .L1
+	mov x0, #0 
+	ldp x19, x20, [sp], #16
+	ldp fp, lr, [sp], #16
+	ret 
+.align 4
+_printi:
+	  stp lr, xzr, [sp, #-16]!
+      mov x1, x0 
+      adr x0, .L._printi_str0 
+      bl printf 
+      mov x0, #0 
+      bl fflush 
+      ldp lr, xzr, [sp], #16
+      ret 
+.align 4
+_println:
+	stp lr, xzr, [sp, #-16]!
+	adr x0, .L._println_str0
+	bl puts
+	mov x0, #0
+	bl fflush
+	ldp lr, xzr, [sp], #16
+	ret
+.align 4
+_readc:
+	 stp x0, lr, [sp, #-16]!
+      mov x1, sp 
+      adr x0, .L._readc_str0 
+      bl scanf 
+      ldp x0, lr, [sp], #16
+      ret
+.align 4
+_readi:
+	 stp x0, lr, [sp, #-16]!
+      mov x1, sp 
+      adr x0, .L._readi_str0 
+      bl scanf 
+      ldp x0, lr, [sp], #16
+      ret 
+.align 4
+_prints:
+	  stp lr, xzr, [sp, #-16]!
+      mov x2, x0 
+      ldrsw x1, [x0, #-4] 
+      adr x0, .L._prints_str0 
+      bl printf 
+      mov x0, #0 
+      bl fflush 
+      ldp lr, xzr, [sp], #16
+      ret 
+
+
+
+

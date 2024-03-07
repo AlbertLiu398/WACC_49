1e1d036ce8cd3f4303ca11f507d9cdfcbf314496
albert <al4922@ic.ac.uk>
2024-03-07 04:50:15 +0000

    feat [peephole]: add comments to list different cases of redundant instructions

--- src/main/wacc/BackEnd/PeepholeOptimisation.scala
+++ src/main/wacc/BackEnd/PeepholeOptimisation.scala
@@ -15,21 +15,119 @@ object PeepholeOptimisation {
     }
 
 
-    // Load-Add
-    // Load-Store
+// -------------peephole size 2---------------
 
-    // Store from a to b 
-    // Load from b to c  ==> Load from a to c
+    // ---------MOVE------------
+
+    /*
+        Redundant Move-Move
+        e.g.
+            mov     w0, w1
+            mov     w2, w0
+        can be replaced with
+            mov     w2, w1
+    */
+
+    /*
+        Redundant Move-Store
+        e.g.
+            mov     w0, w1
+            str     w0, [fp,#28]
+        can be replaced with
+            str     w1, [fp,#28]
+    */
+
+
+    // ---------LOAD------------
+
+    /*
+        Redundant Load-Move
+        e.g.
+            ldr     w0, [fp,#28]
+            mov     w1, w0
+        can be replaced with
+            ldr     w1, [fp,#28]
+    */
+
+
+    /*
+        Redundant Load-Load
+        e.g.
+            ldr     w1, [fp,#28]
+            ldr     w0, [fp,#28]
+        can be replaced with
+            ldr     w1, [fp,#28]
+            mov     w0, w1
+
+    */
+
+    /*
+        Redundant Load-Store
+        e.g.
+            ldr     w0, [fp,#28]
+            str     w0, [fp,#28]
+        can be replaced with
+            ldr     w0, [fp,#28]
+        (remove second instruction)
+    */
+
     
-    def removeRedundantLoads(instrus: List[Instruction]): Unit = {
-        
-        // TODO
-    }
 
-    def removeRedundantStores(instrus: List[Instruction]): Unit = {
-        
-        // TODO
-    }
+    // ---------STORE------------
+
+    /*
+        Redundant Store-Load
+        e.g.
+            str     w0, [fp,#28]
+            ldr     w1, [fp,#28]
+        can be replaced with
+            str     w0, [fp,#28]
+            mov     w1, w0
+        --------------------------
+        e.g. 
+            str     w0, [fp,#28]
+            ldr     w0, [fp,#28]
+        can be replaced with
+            str     w0, [fp,#28]
+        (remove second instruction)
+    */
+
+
+    /*
+        Redundant Store-Store
+        e.g.
+            str     w0, [fp,#28]
+            str     w1, [fp,#28]
+        can be replaced with
+            str     w1, [fp,#28]
+    */
+
+
+
+
+    // ---------STACK------------
+
+
+
+    // ----------ARITHMETIC------------
+
+
+
+
+// -------------peephole size 3---------------
+
+    /* 
+
+        Redundant Move-Move-Move
+        e.g.
+            mov     w0, w1
+            mov     w2, w0
+            mov     w3, w2
+        can be replaced with
+            mov     w3, w1
+
+    */
+
 
     
 

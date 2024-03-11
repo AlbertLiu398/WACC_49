1039a9371d2a20f49264b035b5a89c6e3a4216d9
gs1322 <gs1322@ic.ac.uk>
2024-03-11 22:23:38 +0000

    feat(test): implemented unit test for bitwiseOperator

---
+++ src/test/wacc/BitwiseOperator.scala
@@ -0,0 +1,23 @@
+package wacc
+
+import ast._
+import scala.language.implicitConversions
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import Constant._
+import Instruction._
+import Conditions._
+import org.scalatest._
+import scala.collection._
+import Shift._
+import parser._
+import parsley.{Failure, Result, Success}
+import scala.io.Source
+
+class BitwiseOperator extends AnyFlatSpec with Matchers {
+    it should "parse BitwiseOperator" in {
+        exprParse("1 & 2") shouldBe Success(BitAnd(IntLiter(1),IntLiter(2)))
+        exprParse("1 | 2") shouldBe Success(BitOr(IntLiter(1),IntLiter(2)))
+        exprParse("~2") shouldBe Success(BitNot(IntLiter(2)))
+    }
+}
